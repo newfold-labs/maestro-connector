b3844cbc681344cc72f552da0be48683025da2de
William Earnhardt <wearnhardt@gmail.com>
2020-03-05 09:17:52 -0500

    Adjust HTML structure on add web pro form

--- inc/partials/add.php
+++ inc/partials/add.php
@@ -1,5 +1,10 @@
-<p class="thin"><?php _e( 'Your web professional shared a secret key with you. By entering the key below, you\'re giving them administrative access to your WordPress website. Revoke access at any time.', 'bluehost-maestro' ); ?></p>
-<form method="POST" action="" class="maestro-key-form">
-	<input type="text" name="key" class="key" placeholder="<?php _e( 'Enter secret key here', 'bluehost-maestro' ); ?>" />
-	<button class="maestro-button primary submit"><span class="next"><?php _e( 'Next', 'bluehost-maestro' ); ?></span></button>
-</form>
+<div class="message">
+	<p class="thin"><?php _e( 'Your web professional shared a secret key with you. By entering the key below, you\'re giving them administrative access to your WordPress website. Revoke access at any time.', 'bluehost-maestro' ); ?></p>
+</div>
+<div class="details bold"></div>
+<div class="actions">
+	<form method="POST" action="" class="maestro-key-form">
+		<input type="text" name="key" class="key" placeholder="<?php _e( 'Enter secret key here', 'bluehost-maestro' ); ?>" />
+		<button class="maestro-button primary submit"><span class="next"><?php _e( 'Next', 'bluehost-maestro' ); ?></span></button>
+	</form>
+</div>
