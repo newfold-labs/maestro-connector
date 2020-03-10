f4185f02179d03822b481ce357a22d26df7dfe42
William Earnhardt <wearnhardt@gmail.com>
2020-03-09 22:06:01 -0400

    Delete unused partial

--- inc/partials/requirements.php
+++
@@ -1,6 +0,0 @@
-<p class="thin">
-	<?php _e( 'We\'re sorry, but your site is not compatible with the Maestro plugin.' ); ?>
-</p>
-<p class="thin error">
-	<?php echo esc_html( $compatible->get_error_message() ); ?>
-</p>
