2c826f76338f6d8658eba94206f030fcb5de9fe6
William Earnhardt <wearnhardt@gmail.com>
2020-04-15 06:56:40 -0400

    Fix missed string translations

--- inc/partials/confirm-revoke.php
+++ inc/partials/confirm-revoke.php
@@ -1,4 +1,4 @@
-<p class="thin"><?php _e( 'Are you sure you would like to revoke access for this Web Pro?' ); ?></p>
+<p class="thin"><?php _e( 'Are you sure you would like to revoke access for this Web Pro?', 'maestro-connector' ); ?></p>
 <div class="details bold">
 	<div class='name'><span><?php _e( 'Name', 'maestro-connector' ); ?>:</span> <span><?php echo esc_html( $webpro->user->first_name ); ?> <?php echo esc_html( $webpro->user->last_name ); ?></span></div>
 	<div class='email'><span><?php _e( 'Email', 'maestro-connector' ); ?>:</span> <span><?php echo esc_html( $webpro->user->user_email ); ?></span></div>
