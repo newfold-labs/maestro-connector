8ad732661a8a13f3d2d73394dad943cc027a9902
Amartya Gaur <amarkaushik1999@gmail.com>
2022-09-03 08:28:34 +0530

    fix linting issues add vscode config folder in gitignore

--- inc/RestApi/ThemesController.php
+++ inc/RestApi/ThemesController.php
@@ -14,16 +14,16 @@ use Bluehost\Maestro\Webpro;
  */
 class ThemesController extends \WP_REST_Controller {
 
-    /**
-     * The namespace of this controller's route.
-     * 
-     * @since 1.1.1
-     * 
-     * @var string
-     */
-    protected $namespace = 'bluehost/maestro/v1';
+	/**
+	 * The namespace of this controller's route.
+	 *
+	 * @since 1.1.1
+	 *
+	 * @var string
+	 */
+	protected $namespace = 'bluehost/maestro/v1';
 
-    /**
+	/**
 	 * The current Web Pro accessing the endpoint
 	 *
 	 * @since 1.1.1
@@ -32,12 +32,12 @@ class ThemesController extends \WP_REST_Controller {
 	 */
 	private $webpro;
 
-    /**
-     * Regusters the Themes routes
-     * 
-     * @since 1.1.1
-     */
-    public function register_routes() {
+	/**
+	 * Registers the Themes routes
+	 *
+	 * @since 1.1.1
+	 */
+	public function register_routes() {
 
 		register_rest_route(
 			$this->namespace,
@@ -53,40 +53,38 @@ class ThemesController extends \WP_REST_Controller {
 
 	}
 
-    /**
-     * Callback for the themes get endpoint
-     * 
-     * Returns a list of installed themes with id, name, title
-     * status, version, update, update_version and screenshot
-     * 
-     * @since 1.1.1
-     * 
-     * @return WP_Rest_Response Returns a standard rest response with a list of themes
-     */
-    public function get_themes() {
-        $themes_list = array();
-        $themes_installed = wp_get_themes();
+	/**
+	 * Callback for the themes get endpoint
+	 *
+	 * Returns a list of installed themes with id, name, title
+	 * status, version, update, update_version and screenshot
+	 *
+	 * @since 1.1.1
+	 *
+	 * @return WP_Rest_Response Returns a standard rest response with a list of themes
+	 */
+	public function get_themes() {
+		$themes_list      = array();
+		$themes_installed = wp_get_themes();
 
-        foreach ( $themes_installed as $theme_id => $theme_wp ) {
-            $theme = new Theme( $theme_id, $theme_wp );
-            array_push( $themes_list, $theme );
-        }
+		foreach ( $themes_installed as $theme_id => $theme_wp ) {
+			$theme = new Theme( $theme_id, $theme_wp );
+			array_push( $themes_list, $theme );
+		}
+		$response = array( $themes_list );
+		return new WP_Rest_Response( $response );
+	}
 
-        $response = array( $themes_list );
-
-        return new WP_Rest_Response( $response );
-    }
-
-    /**
-     * Verify permission to access this endpoint
-     * 
-     * Autheinticating a Webpro user via token 
-     * 
-     * @since 1.1.1
-     * 
-     * @return boolean Whether to allow access to endpoint.
-     */
-    public function check_permission() {
+	/**
+	 * Verify permission to access this endpoint
+	 *
+	 * Autheinticating a Webpro user via token
+	 *
+	 * @since 1.1.1
+	 *
+	 * @return boolean Whether to allow access to endpoint.
+	 */
+	public function check_permission() {
 
 		// We want to SSO into the same user making the current request
 		// User is also already verified as a Maestro using the permission callback
--- inc/Theme.php
+++ inc/Theme.php
@@ -10,124 +10,124 @@ use WP_User_Query;
  */
 class Theme {
 
-    /**
-     * The id for the theme
-     * 
-     * @since 1.1.1
-     * 
-     * @var string
-     */
-    public $id;
+	/**
+	 * The id for the theme
+	 *
+	 * @since 1.1.1
+	 *
+	 * @var string
+	 */
+	public $id;
 
-    /**
-     * The theme name, usually same as the id
-     * 
-     * @since 1.1.1
-     * 
-     * @var string
-     */
-    public $name;
+	/**
+	 * The theme name, usually same as the id
+	 *
+	 * @since 1.1.1
+	 *
+	 * @var string
+	 */
+	public $name;
 
-    /**
-     * The theme's title (or Name in pure WP sense)
-     * 
-     * @since 1.1.1
-     * 
-     * @var string
-     */
-    public $title;
+	/**
+	 * The theme's title (or Name in pure WP sense)
+	 *
+	 * @since 1.1.1
+	 *
+	 * @var string
+	 */
+	public $title;
 
-    /**
-     * Theme's status, will either be active or inactive
-     * 
-     * @since 1.1.1
-     * 
-     * @var string
-     */
-    public $status;
+	/**
+	 * Theme's status, will either be active or inactive
+	 *
+	 * @since 1.1.1
+	 *
+	 * @var string
+	 */
+	public $status;
 
-    /**
-     * Theme's version
-     * 
-     * @since 1.1.1
-     * 
-     * @var string
-     */
-    public $version;
+	/**
+	 * Theme's version
+	 *
+	 * @since 1.1.1
+	 *
+	 * @var string
+	 */
+	public $version;
 
-    /**
-     * Whether there is an update available for the theme or not
-     * will be either none or available
-     * 
-     * @since 1.1.1
-     * 
-     * @var string
-     */
-    public $update;
+	/**
+	 * Whether there is an update available for the theme or not
+	 * will be either none or available
+	 *
+	 * @since 1.1.1
+	 *
+	 * @var string
+	 */
+	public $update;
 
-    /**
-     * Theme's update version, will be either (undef) or the version
-     * 
-     * @since 1.1.1
-     * 
-     * @var string
-     */
-    public $update_version;
+	/**
+	 * Theme's update version, will be either (undef) or the version
+	 *
+	 * @since 1.1.1
+	 *
+	 * @var string
+	 */
+	public $update_version;
 
-    /**
-     * Theme's screenshot, an associative array of file and url
-     * 
-     * @since 1.1.1
-     * 
-     * @var array
-     */
-    public $screenshot;
+	/**
+	 * Theme's screenshot, an associative array of file and url
+	 *
+	 * @since 1.1.1
+	 *
+	 * @var array
+	 */
+	public $screenshot;
 
 
-    /**
-     * Constructor
-     * 
-     * @since 1.1.1
-     * 
-     * @param theme_id string   The theme id
-     * @param theme    WP_Theme object to initialize our theme
-     */
-    public function __construct( $theme_id, $theme ) {
-        // Make sure we populate the themes updates transient
-        wp_update_themes();
+	/**
+	 * Constructor
+	 *
+	 * @since 1.1.1
+	 *
+	 * @param string   $theme_id The theme id
+	 * @param WP_Theme $theme    object to initialize our theme
+	 */
+	public function __construct( $theme_id, $theme ) {
+		// Make sure we populate the themes updates transient
+		wp_update_themes();
 
-        // Get the current theme
-        $current_theme = wp_get_theme();
+		// Get the current theme
+		$current_theme = wp_get_theme();
 
-        // Include theme functions
-        require_once( ABSPATH . 'wp-admin/includes/theme.php' );
+		// Include theme functions
+		require_once ABSPATH . 'wp-admin/includes/theme.php';
 
-        $theme_updates  = get_site_transient( 'update_themes' );
-        $stylesheet     = $theme->get_stylesheet();
-        $update         = 'none';
-        $update_version = '(undef)';
-        if ( array_key_exists( $stylesheet, $theme_updates->response ) ) {
-            $update         = 'available';
-            $update_version = $themes_updates->response[ $stylesheet ]['new_version'];
-        }
+		$theme_updates  = get_site_transient( 'update_themes' );
+		$stylesheet     = $theme->get_stylesheet();
+		$update         = 'none';
+		$update_version = '(undef)';
+		if ( array_key_exists( $stylesheet, $theme_updates->response ) ) {
+			$update         = 'available';
+			$update_version = $themes_updates->response[ $stylesheet ]['new_version'];
+		}
 
-        $screenshot_url       = $theme->get_screenshot() ? $theme->get_screenshot() : 'none';
-        $screenshot_url_array = $screenshot_url != 'none' ? explode( '/', $screenshot_url ) : array( 'none' );
-        $filename             = end( $screenshot_url_array );
+		$screenshot_url       = $theme->get_screenshot() ? $theme->get_screenshot() : 'none';
+		$screenshot_url_array = 'none' !== $screenshot_url ? explode( '/', $screenshot_url ) : array( 'none' );
+		$filename             = end( $screenshot_url_array );
 
-        $screenshot = array(
-            'url'  => $screenshot_url,
-            'file' => $filename
-        );
+		$screenshot = array(
+			'url'  => $screenshot_url,
+			'file' => $filename,
+		);
 
-        // Assign all the required values
-        $this->id             = $theme_id;
-        $this->name           = $theme_id;
-        $this->title          = $theme->display( 'Name' );
-        $this->status         = $theme->display( 'Name' ) == $current_theme->display( 'Name' ) ? 'active' : 'inactive';
-        $this->version        = $theme->get('Version');
-        $this->update         = $update;
-        $this->update_version = $update_version;
-        $this->screenshot     = $screenshot;
-    }
+		// Assign all the required values
+		$this->id             = $theme_id;
+		$this->name           = $theme_id;
+		$this->title          = $theme->display( 'Name' );
+		$this->status         = $theme->display( 'Name' ) === $current_theme->display( 'Name' ) ? 'active' : 'inactive';
+		$this->version        = $theme->get( 'Version' );
+		$this->update         = $update;
+		$this->update_version = $update_version;
+		$this->screenshot     = $screenshot;
+	}
 }
